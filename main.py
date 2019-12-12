10d70da39674fa13bbfeccb1b1202c0d9f06f93b
Antonina <antonina.glazhevska@gmail.com>
2019-12-12 10:33:59 +0200

    added source files

---
+++ main.py
@@ -0,0 +1,26 @@
+def getLetter(num):
+    # Согласно таблице ASCII - код 65 = A
+    offset = 65 
+    # Функция chr преобразует число в символ
+    remainder = num % 26
+    return chr(offset + remainder)
+
+# Принимает номер столбца в качестве аргумента и
+# возвращает имя столбца как в Экселе.
+    
+    
+def convert(num):
+    if num < 26:
+        return getLetter(num)
+    if num < 676:
+        result = num // 26
+        print (result)
+        return getLetter(result) + getLetter(num)
+    result1 = num // 26
+    result2 = result1 // 26
+    return getLetter(result2) + getLetter(result1)+ getLetter(num)
+
+def printAscii():
+    for n in range(0, 256):
+        print("%d = %s" % (n, chr(n)))
+    #    print (str(n) + " = " + chr(n))
---
+++ test.py
@@ -0,0 +1,31 @@
+import unittest
+from main import convert, getLetter
+
+class A1NotationTest(unittest.TestCase):
+    def test_getLetter(self):
+        self.assertEqual(getLetter(0), 'A')
+        self.assertEqual(getLetter(1), 'B')
+        self.assertEqual(getLetter(25), 'Z')
+        self.assertEqual(getLetter(26), 'A')
+        self.assertEqual(getLetter(27), 'B')
+        self.assertEqual(getLetter(51), 'Z')
+        self.assertEqual(getLetter(52), 'A')
+        
+    def test_convert(self):
+        self.assertEqual(convert(0), 'A')
+        self.assertEqual(convert(1), 'B')
+        self.assertEqual(convert(25), 'Z')
+        self.assertEqual(convert(26), 'BA')
+        self.assertEqual(convert(27), 'BB')
+        self.assertEqual(convert(28), 'BC')
+        self.assertEqual(convert(51), 'BZ')
+        self.assertEqual(convert(52), 'CA')
+        self.assertEqual(convert(675), 'ZZ')
+        self.assertEqual(convert(676), 'BAA')
+        self.assertEqual(convert(690), 'BAO')
+        self.assertEqual(convert(701), 'BAZ')
+        self.assertEqual(convert(702), 'BBA')
+        self.assertEqual(convert(703), 'BBB')
+
+if __name__ == '__main__':
+    unittest.main()
