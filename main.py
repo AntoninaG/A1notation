e1d270cd420e54b73e3821b2e52ac2f0056152f0
Viacheslav Glazhevskyi <glajik@gmail.com>
2019-12-12 19:17:20 +0200

    Tried to use strings methods to get result, but test is failed

--- main.py
+++ main.py
@@ -11,3 +11,15 @@ def convert(num):
     if num < 26:
         return getLetter(num)
     return convert(num // 26) + getLetter(num)
+
+def shift(letter):
+    num = ord(letter) - 1
+    return chr(num)
+
+def getA1Notation(num):
+    text = convert(num - 1)
+    first = text[0]
+    rest = text[1:]
+    if not rest:
+        return text
+    return shift(first) + rest 
--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 import unittest
-from main import convert, getLetter
+from main import convert, getLetter, getA1Notation
 
 class A1NotationTest(unittest.TestCase):
-    def test_getLetter(self):
+    def test1_getLetter(self):
         self.assertEqual(getLetter(0), 'A')
         self.assertEqual(getLetter(1), 'B')
         self.assertEqual(getLetter(25), 'Z')
@@ -11,7 +11,7 @@ class A1NotationTest(unittest.TestCase):
         self.assertEqual(getLetter(51), 'Z')
         self.assertEqual(getLetter(52), 'A')
         
-    def test_convert(self):
+    def test2_convert(self):
         self.assertEqual(convert(0), 'A')
         self.assertEqual(convert(1), 'B')
         self.assertEqual(convert(25), 'Z')
@@ -27,6 +27,25 @@ class A1NotationTest(unittest.TestCase):
         self.assertEqual(convert(701), 'BAZ')
         self.assertEqual(convert(702), 'BBA')
         self.assertEqual(convert(703), 'BBB')
+
+    def test3_getA1Notation(self):
+        self.assertEqual(getA1Notation(1), 'A')
+        self.assertEqual(getA1Notation(2), 'B')
+        self.assertEqual(getA1Notation(25), 'Y')
+        self.assertEqual(getA1Notation(26), 'Z')
+
+        self.assertEqual(getA1Notation(27), 'AA')
+        self.assertEqual(getA1Notation(28), 'AB')
+        self.assertEqual(getA1Notation(51), 'AY')
+        self.assertEqual(getA1Notation(52), 'AZ')
+        self.assertEqual(getA1Notation(53), 'BA')
+        self.assertEqual(getA1Notation(54), 'BB')
+
+        self.assertEqual(getA1Notation(675), 'YY')
+        self.assertEqual(getA1Notation(676), 'YZ')
+        self.assertEqual(getA1Notation(677), 'ZA')
+        self.assertEqual(getA1Notation(678), 'ZB')
+
 
 if __name__ == '__main__':
     unittest.main()
