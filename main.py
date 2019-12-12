7da95d8e23629071b7cb29debdaa598a3bfdb803
Antonina <antonina.glazhevska@gmail.com>
2019-12-12 15:00:03 +0200

    Deleted old functions

--- main.py
+++ main.py
@@ -11,19 +11,3 @@ def convert(num):
     if num < 26:
         return getLetter(num)
     return convert(num // 26) + getLetter(num)
-
-    
-def convert_old(num):
-    if num < 26:
-        return getLetter(num)
-    if num < 676:
-        result = num // 26
-        return getLetter(result) + getLetter(num)
-    result1 = num // 26
-    result2 = result1 // 26
-    return getLetter(result2) + getLetter(result1)+ getLetter(num)
-
-def printAscii():
-    for n in range(0, 256):
-        print("%d = %s" % (n, chr(n)))
-    #    print (str(n) + " = " + chr(n))
